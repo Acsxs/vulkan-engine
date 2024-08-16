7f24c59bb780aec1aa9b4d7eef05002ed89b70d2
Quan <quan.hoan1112@gmail.com>
2024-08-16 14:29:42 +0930

    device

---
+++ bin/src/vk_device.cpp
@@ -0,0 +1,166 @@
+#include "vk_device.h"
+
+
+void VulkanDevice::init(vkb::PhysicalDevice vkbPhysicalDevice, VkInstance instance) {
+
+	vkb::DeviceBuilder vkbDeviceBuilder{ vkbPhysicalDevice };
+
+	vkb::Device vkbDevice = vkbDeviceBuilder.build().value();
+
+	_logicalDevice = vkbDevice.device;
+	_physicalDevice = vkbPhysicalDevice.physical_device;
+
+	_queues[0] = vkbDevice.get_queue(vkb::QueueType::graphics).value();
+	_queueFamilies[0] = vkbDevice.get_queue_index(vkb::QueueType::graphics).value();
+
+	_queues[1] = vkbDevice.get_queue(vkb::QueueType::transfer).value();
+	_queueFamilies[1] = vkbDevice.get_queue_index(vkb::QueueType::transfer).value();
+
+	_queues[2] = vkbDevice.get_queue(vkb::QueueType::compute).value();
+	_queueFamilies[2] = vkbDevice.get_queue_index(vkb::QueueType::compute).value();
+
+	VmaAllocatorCreateInfo allocatorCreateInfo = {};
+	allocatorCreateInfo.physicalDevice = _physicalDevice;
+	allocatorCreateInfo.device = _logicalDevice;
+	allocatorCreateInfo.instance = instance;
+	allocatorCreateInfo.flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT;
+	vmaCreateAllocator(&allocatorCreateInfo, &_allocator);
+
+	VkCommandPoolCreateInfo commandPoolCreateInfo = vkinit::CommandPoolCreateInfo(getQueueFamilyIndex(GRAPHICS), VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
+	VK_CHECK(vkCreateCommandPool(_logicalDevice, &commandPoolCreateInfo, nullptr, &_immCommandPool));
+
+	VkCommandBufferAllocateInfo commandBufferAllocateInfo = vkinit::CommandBufferAllocateInfo(_immCommandPool, 1);
+	VK_CHECK(vkAllocateCommandBuffers(_logicalDevice, &commandBufferAllocateInfo, &_immCommandBuffer));
+}
+
+void VulkanDevice::destroy() {
+	vkDeviceWaitIdle(_logicalDevice);
+	vkDestroyCommandPool(_logicalDevice, _immCommandPool, nullptr);
+	vmaDestroyAllocator(_allocator);
+	vkDestroyDevice(_logicalDevice, nullptr);
+}
+
+AllocatedBuffer VulkanDevice::createBuffer(size_t allocSize, VkBufferUsageFlags usage, VmaMemoryUsage memoryUsage) {
+
+	// allocate buffer
+	VkBufferCreateInfo bufferCreateInfo = { .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO };
+	bufferCreateInfo.pNext = nullptr;
+	bufferCreateInfo.size = allocSize;
+
+	bufferCreateInfo.usage = usage;
+
+	VmaAllocationCreateInfo bufferAllocationCreateInfo = {};
+	bufferAllocationCreateInfo.usage = memoryUsage;
+	bufferAllocationCreateInfo.flags = VMA_ALLOCATION_CREATE_MAPPED_BIT;
+	AllocatedBuffer newBuffer;
+
+	// allocate the buffer
+	VK_CHECK(vmaCreateBuffer(_allocator, &bufferCreateInfo, &bufferAllocationCreateInfo, &newBuffer.buffer, &newBuffer.allocation,
+		&newBuffer.info));
+
+	return newBuffer;
+}
+
+void VulkanDevice::destroyBuffer(const AllocatedBuffer& buffer) {
+	vmaDestroyBuffer(_allocator, buffer.buffer, buffer.allocation);
+}
+
+
+AllocatedImage VulkanDevice::createImage(VkExtent3D size, VkFormat format, VkImageUsageFlags usage, VkImageAspectFlags aspect, bool mipmapped)
+{
+	AllocatedImage newImage;
+	newImage.imageFormat = format;
+	newImage.imageExtent = size;
+
+	VkImageCreateInfo img_info = vkinit::ImageCreateInfo(format, usage, size);
+	if (mipmapped) {
+		img_info.mipLevels = static_cast<uint32_t>(std::floor(std::log2(std::max(size.width, size.height)))) + 1;
+	}
+
+	// always allocate images on dedicated GPU memory
+	VmaAllocationCreateInfo allocinfo = {};
+	allocinfo.usage = VMA_MEMORY_USAGE_GPU_ONLY;
+	allocinfo.requiredFlags = VkMemoryPropertyFlags(VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+
+	// allocate and create the image
+	VK_CHECK(vmaCreateImage(_allocator, &img_info, &allocinfo, &newImage.image, &newImage.allocation, nullptr));
+
+	// if the format is a depth format, we will need to have it use the correct
+	// aspect flag
+	//VkImageAspectFlags aspectFlag = VK_IMAGE_ASPECT_COLOR_BIT;
+	//if (format == VK_FORMAT_D32_SFLOAT) {
+	//	aspectFlag = VK_IMAGE_ASPECT_DEPTH_BIT;
+	//}
+
+	// build a image-view for the image
+	VkImageViewCreateInfo view_info = vkinit::ImageViewCreateInfo(format, newImage.image, aspect);
+	view_info.subresourceRange.levelCount = img_info.mipLevels;
+
+	VK_CHECK(vkCreateImageView(_logicalDevice, &view_info, nullptr, &newImage.imageView));
+
+	return newImage;
+}
+
+AllocatedImage VulkanDevice::createImage(void* data, VkExtent3D size, VkFormat format, VkImageUsageFlags usage, VkImageAspectFlags aspect, bool mipmapped)
+{
+	size_t data_size = size.depth * size.width * size.height * 4;
+	AllocatedBuffer uploadbuffer = createBuffer(data_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VMA_MEMORY_USAGE_CPU_TO_GPU);
+
+	memcpy(uploadbuffer.info.pMappedData, data, data_size);
+
+	AllocatedImage new_image = createImage(size, format, usage | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT, mipmapped);
+
+	immediateSubmit([&](VkCommandBuffer* commandBuffer) {
+		vkutil::transitionImage(commandBuffer, &new_image.image, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+
+		VkBufferImageCopy copyRegion = {};
+		copyRegion.bufferOffset = 0;
+		copyRegion.bufferRowLength = 0;
+		copyRegion.bufferImageHeight = 0;
+
+		copyRegion.imageSubresource.aspectMask = aspect;
+		copyRegion.imageSubresource.mipLevel = 0;
+		copyRegion.imageSubresource.baseArrayLayer = 0;
+		copyRegion.imageSubresource.layerCount = 1;
+		copyRegion.imageExtent = size;
+
+		// copy the buffer into the image
+		vkCmdCopyBufferToImage(*commandBuffer, uploadbuffer.buffer, new_image.image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &copyRegion);
+
+		vkutil::transitionImage(commandBuffer, &new_image.image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+			VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+		});
+
+	destroyBuffer(uploadbuffer);
+
+	return new_image;
+}
+
+void VulkanDevice::destroyImage(const AllocatedImage& img)
+{
+	vkDestroyImageView(_logicalDevice, img.imageView, nullptr);
+	vmaDestroyImage(_allocator, img.image, img.allocation);
+}
+
+
+
+void VulkanDevice::immediateSubmit(std::function<void(VkCommandBuffer* commandBuffer)>&& function)
+{
+	VK_CHECK(vkResetFences(_logicalDevice, 1, &_immFence));
+	VK_CHECK(vkResetCommandBuffer(_immCommandBuffer, 0));
+
+	VkCommandBufferBeginInfo cmdBeginInfo = vkinit::CommandBufferBeginInfo(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
+
+	VK_CHECK(vkBeginCommandBuffer(_immCommandBuffer, &cmdBeginInfo));
+
+	function(&_immCommandBuffer);
+
+	VK_CHECK(vkEndCommandBuffer(_immCommandBuffer));
+
+	VkCommandBufferSubmitInfo commandBufferSubmitInfo = vkinit::CommandBufferSubmitInfo(_immCommandBuffer);
+	VkSubmitInfo2 submitInfo = vkinit::SubmitInfo2(&commandBufferSubmitInfo, nullptr, nullptr);
+
+	VK_CHECK(vkQueueSubmit2(getQueue(GRAPHICS), 1, &submitInfo, _immFence));
+
+	VK_CHECK(vkWaitForFences(_logicalDevice, 1, &_immFence, true, 9999999999));
+}
---
+++ bin/src/vk_device.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <vk_types.h>
+#include <VkBootstrap.h>
+#include "vk_initializers.h"
+#include "vk_images.h"
+
+
+
+class VulkanDevice {
+public:
+	enum queueIndices {
+		GRAPHICS,
+		TRANSFER,
+		COMPUTE
+	};
+	VmaAllocator _allocator;
+
+	VkFence _immFence;
+	VkCommandBuffer _immCommandBuffer;
+	VkCommandPool _immCommandPool;
+
+	VkPhysicalDevice _physicalDevice;
+	VkDevice _logicalDevice;
+	uint32_t _queueFamilies[3];
+	VkQueue _queues[3];
+
+	VkQueue getQueue(uint8_t queueIndex) { return _queues[queueIndex]; };
+	uint32_t getQueueFamilyIndex(uint8_t queueIndex) { return _queueFamilies[queueIndex]; };
+
+	AllocatedBuffer createBuffer(size_t allocSize, VkBufferUsageFlags usage, VmaMemoryUsage memoryUsage);
+	void destroyBuffer(const AllocatedBuffer& buffer);
+
+	AllocatedImage createImage(VkExtent3D size, VkFormat format, VkImageUsageFlags usage, VkImageAspectFlags aspectFlags, bool mipmapped = false);
+	AllocatedImage createImage(void* data, VkExtent3D size, VkFormat format, VkImageUsageFlags usage, VkImageAspectFlags aspectFlags, bool mipmapped = false);
+	void destroyImage(const AllocatedImage& img);
+
+	void immediateSubmit(std::function<void(VkCommandBuffer* commandBuffer)>&& function);
+
+	void destroy();
+	void init(vkb::PhysicalDevice physicalDevice, VkInstance instance);
+
+};
