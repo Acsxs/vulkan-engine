0b774db7a52548a4b49bc33fcb74416f21d4b8b8
Quan <quan.hoan1112@gmail.com>
2024-08-26 15:02:51 +0930

    tinygltf

--- src/vk_loader.cpp
+++ src/vk_loader.cpp
@@ -619,3 +619,26 @@ void LoadedGLTF::clearAll() {
         vkDestroySampler(dv, sampler, nullptr);
     }
 }
+
+bool loadModel(tinygltf::Model* model, std::string filePath) {
+    fmt::println ("Loading GLTF: {}", filePath);
+    
+    tinygltf::TinyGLTF tinyGLTF;
+
+    std::string err;
+    std::string warn;
+
+    bool res = tinyGLTF.LoadASCIIFromFile(model, &err, &warn, filePath);
+    if (!warn.empty()) {
+        std::cout << "WARN: " << warn << std::endl;
+    }
+
+    if (!err.empty()) {
+        std::cout << "ERR: " << err << std::endl;
+    }
+    if (!res)
+        std::cout << "Failed to load glTF: " << filePath << std::endl;
+    else
+        std::cout << "Loaded glTF: " << filePath << std::endl;
+    return res;
+}
