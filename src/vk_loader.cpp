01c690178eefe0996c7fd7c35366d3d9687fb6e7
Quan <quan.hoan1112@gmail.com>
2024-09-11 13:03:09 +0930

    simpler stb include

--- src/vk_loader.cpp
+++ src/vk_loader.cpp
@@ -1,11 +1,7 @@
 ﻿
 #define STB_IMAGE_IMPLEMENTATION 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image.h"
-
-
 #define TINYGLTF_IMPLEMENTATION
-#define TINYGLTF_NO_INCLUDE_STB_IMAGE
 #include "tiny_gltf.h"
 
 #include <iostream>
